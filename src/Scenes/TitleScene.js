0eb3a75689f269fb7494547db177a8fafea09a56
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-10-24 08:45:02 +0100

    made update to stop and restart the the background music

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -56,9 +56,10 @@ this.input.on('pointerout', function (event, gameObjects) {
 
 this.model = this.sys.game.globals.model;
 if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
-  this.model.bgMusicPlaying = true;
   this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
   this.bgMusic.play();
+  this.model.bgMusicPlaying = true;
+  this.sys.game.globals.bgMusic = this.bgMusic;
 }
 
   }
